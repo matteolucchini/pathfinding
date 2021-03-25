25786e814f53876662c7429e72e20ee053609e74
matteolucchini <matteo.lucchini97@gmail.com>
2021-03-25 16:07:31 +0100

    aggiunto il concetto di pair

--- main.c
+++ main.c
@@ -8,28 +8,49 @@
 #define MIN_COST 1
 #define BLOCK_NODE -1
 
+// Dichiaro coppie per rappresentare il punto con le sue coordinate
+typedef struct pair {
+  int x;
+  int y;
+} Pair;
+
 // Dichiarazione struttura nodo
 typedef struct node {
   float cost;
-  struct Node *near_nodes[];
+  Pair nearNodes[];
 } Node;
 
 void randomCost(Node grid[ROW][COL]){
   for(int i = 0; i < ROW; i++)
     for(int j = 0; j < COL; j++) {
-      if((rand() & 1) | (rand() & 1)) // gives 1 with probability of 75%, gives 0 with probability of 25%
-        // if 1
-        grid[i][j].cost = MIN_COST + (float)(rand()) / (float)(RAND_MAX / (MAX_COST - MIN_COST)); // random float number from 0 to MAX_COST
-      else
-        // if 0
-        grid[i][j].cost = BLOCK_NODE;
+        if((rand() & 1) | (rand() & 1)) { // gives 1 with probability of 75%, gives 0 with probability of 25%
+          // if 1
+          grid[i][j].cost = MIN_COST + (float)(rand()) / (float)(RAND_MAX / (MAX_COST - MIN_COST)); // random float number from 0 to MAX_COST
+        }
+        else {
+          // if 0
+          grid[i][j].cost = BLOCK_NODE;
+        }
       }
 
+      /*
       for(int i = 0; i < ROW; i++) {
         for(int j = 0; j < COL; j++)
             printf("%.2f \t", grid[i][j].cost);
         printf("\n");
       }
+      */
+}
+
+/*
+void setNearNodes(Node grid[ROW][COL]) {
+  for(int i = 0; i < ROW; i++)
+    for(int j = 0; j < COL; j++)
+}
+*/
+
+void aStarSearch(Node grid[ROW][COL], Node src, Node dst) {
+
 }
 
 int main(int argc, char *argv[]) {
